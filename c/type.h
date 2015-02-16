b4a688bd755b14305bae72e214246cf3cd496a23
Philipp Paulweber <p.paulweber@gmail.com>
2015-02-16 15:29:41 +0100

    C Types
    * added a new C type header which includes general types
    * also this header includes basic c headers

---
+++ c/type.h
@@ -0,0 +1,59 @@
+
+/*
+  Copyright (C) 2015 Philipp Paulweber
+  
+  This file is part of the 'libstdhl' project which is released under a NSCA
+  open source software license. For more information, see the LICENSE.txt
+  file in the project root directory.
+*/
+
+#ifndef _LIB_STDHL_TYPE_H_
+#define _LIB_STDHL_TYPE_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+#include <assert.h>
+
+/**
+   @file     Type.h
+   @class    Type
+   
+   @brief    TODO
+   
+   TODO
+   
+   @author   Philipp Paulweber
+   @date     2015-02-14
+*/
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint16_t u32;
+typedef uint64_t u64;
+
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int16_t  i32;
+typedef int64_t  i64;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LIB_STDHL_TYPE_H_ */
+
+
+/*
+  Local variables:
+  mode: c++
+  indent-tabs-mode: t
+  c-basic-offset: 4
+  tab-width: 4
+  End:
+  vim:noexpandtab:sw=4:ts=4:
+*/
