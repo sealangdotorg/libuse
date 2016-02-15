2ff355f719a3defc6fd27b0177b7eab0f2a9d3b2
Philipp Paulweber <p.paulweber@gmail.com>
2016-02-16 00:36:20 +0100

    Binding Interface
    * added new aid interface for binding a arbitrary value to a class

---
+++ cpp/Binding.h
@@ -0,0 +1,91 @@
+//  
+//  Copyright (c) 2016 Philipp Paulweber
+//  All rights reserved.
+//  
+//  Developed by: Philipp Paulweber
+//                https://github.com/ppaulweber/libstdhl
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a 
+//  copy of this software and associated documentation files (the "Software"), 
+//  to deal with the Software without restriction, including without limitation 
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+//  and/or sell copies of the Software, and to permit persons to whom the 
+//  Software is furnished to do so, subject to the following conditions:
+//  
+//  * Redistributions of source code must retain the above copyright 
+//    notice, this list of conditions and the following disclaimers.
+//  
+//  * Redistributions in binary form must reproduce the above copyright 
+//    notice, this list of conditions and the following disclaimers in the 
+//    documentation and/or other materials provided with the distribution.
+//  
+//  * Neither the names of the copyright holders, nor the names of its 
+//    contributors may be used to endorse or promote products derived from 
+//    this Software without specific prior written permission.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//  CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+//  WITH THE SOFTWARE.
+//  
+
+#ifndef _LIB_STDHL_CPP_BINDING_H_
+#define _LIB_STDHL_CPP_BINDING_H_
+
+#include "Type.h"
+
+/**
+   @brief    TODO
+   
+   TODO
+*/
+
+namespace libstdhl 
+{
+    template< class C >
+	class Binding
+	{
+	private:
+		C* reference;
+		
+	public:
+		void bind( C* object )
+		{
+			assert( !reference && "already bounded to a reference" );
+			assert( object && "invalid object reference to bind" );
+			
+			reference = object;
+		}
+		
+		C* unbind( void )
+		{
+			assert( reference && "object reference was never bound" );
+			
+			C* tmp = reference;
+			reference = 0;
+			return tmp;
+		}
+		
+	    C* getBound( void ) const
+		{
+			assert( reference && "object reference was never bound" );
+			return reference;
+		}
+	};
+}
+
+#endif /* _LIB_STDHL_CPP_BINDING_H_ */
+
+
+//  
+//  Local variables:
+//  mode: c++
+//  indent-tabs-mode: t
+//  c-basic-offset: 4
+//  tab-width: 4
+//  End:
+//  vim:noexpandtab:sw=4:ts=4:
+//  
