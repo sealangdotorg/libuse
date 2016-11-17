f8b57bfed8fdd8385fa3395907da8608a26b4726
Philipp Paulweber <p.paulweber@gmail.com>
2016-11-17 11:45:43 +0100

    License
    * updated license information for all source files

--- cpp/Default.h
+++ cpp/Default.h
@@ -1,25 +1,25 @@
-//  
+//
 //  Copyright (c) 2014-2016 Philipp Paulweber
 //  All rights reserved.
-//  
+//
 //  Developed by: Philipp Paulweber
 //                https://github.com/ppaulweber/libstdhl
-//  
+//
 //  This file is part of libstdhl.
-//  
+//
 //  libstdhl is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-//  
+//
 //  libstdhl is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 //  GNU General Public License for more details.
-//  
+//
 //  You should have received a copy of the GNU General Public License
 //  along with libstdhl. If not, see <http://www.gnu.org/licenses/>.
-//  
+//
 
 #ifndef _LIB_STDHL_CPP_DEFAULT_H_
 #define _LIB_STDHL_CPP_DEFAULT_H_
