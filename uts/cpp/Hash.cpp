57660eedfc2183586c263b99e99f943f9448492a
Philipp Paulweber <p.paulweber@gmail.com>
2017-05-12 12:20:14 +0200

    Hash
    * started with code hashing functionality and abstraction
    * added hash value combine function (discovered by @emmanuel099)

--- uts/cpp/Hash.cpp
+++ uts/cpp/Hash.cpp
@@ -22,55 +22,15 @@
 //  along with libstdhl. If not, see <http://www.gnu.org/licenses/>.
 //
 
-#ifndef _LIB_STDHL_H_
-#define _LIB_STDHL_H_
+#include "uts/main.h"
 
-/**
-   @brief    TODO
+using namespace libstdhl;
 
-   TODO
-*/
-
-#ifndef __cplusplus
-
-// C includes
-
-#include "c/args.h"
-#include "c/default.h"
-#include "c/type.h"
-
-#else // __cplusplus
-
-// C++ includes
-
-#include "cpp/Allocator.h"
-#include "cpp/Ansi.h"
-#include "cpp/Args.h"
-#include "cpp/Binding.h"
-#include "cpp/Default.h"
-#include "cpp/Enum.h"
-#include "cpp/File.h"
-#include "cpp/FloatingPoint.h"
-#include "cpp/Hash.h"
-#include "cpp/Integer.h"
-#include "cpp/Json.h"
-#include "cpp/Labeling.h"
-#include "cpp/List.h"
-#include "cpp/Log.h"
-#include "cpp/Random.h"
-#include "cpp/Rational.h"
-#include "cpp/String.h"
-#include "cpp/Type.h"
-#include "cpp/Variadic.h"
-#include "cpp/Xml.h"
-
-namespace libstdhl
+TEST( libstdhl_cpp_Hash, example )
 {
+    EXPECT_EQ( libstdhl::Hash::combine( 123, 456 ),
+        libstdhl::Hash::combine( 123, 456 ) );
 }
-
-#endif // __cplusplus
-
-#endif // _LIB_STDHL_H_
 
 //
 //  Local variables:
