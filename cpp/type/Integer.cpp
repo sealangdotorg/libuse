45ba849cb21bbd36fa1962c9a97e15a277024c47
Philipp Paulweber <p.paulweber@gmail.com>
2017-08-30 14:08:07 +0200

    License
    * updated README information about copyright holder and license info
    * license changed from GPLv3+ to GPLv3+ with an additional permission in
      form of a GPL linking exception

--- cpp/type/Integer.cpp
+++ cpp/type/Integer.cpp
@@ -21,6 +21,24 @@
 //  You should have received a copy of the GNU General Public License
 //  along with libstdhl. If not, see <http://www.gnu.org/licenses/>.
 //
+//  Additional permission under GNU GPL version 3 section 7
+//
+//  libstdhl is distributed under the terms of the GNU General Public License
+//  with the following clarification and special exception: Linking libstdhl
+//  statically or dynamically with other modules is making a combined work
+//  based on libstdhl. Thus, the terms and conditions of the GNU General
+//  Public License cover the whole combination. As a special exception,
+//  the copyright holders of libstdhl give you permission to link libstdhl
+//  with independent modules to produce an executable, regardless of the
+//  license terms of these independent modules, and to copy and distribute
+//  the resulting executable under terms of your choice, provided that you
+//  also meet, for each linked independent module, the terms and conditions
+//  of the license of that module. An independent module is a module which
+//  is not derived from or based on libstdhl. If you modify libstdhl, you
+//  may extend this exception to your version of the library, but you are
+//  not obliged to do so. If you do not wish to do so, delete this exception
+//  statement from your version.
+//
 
 #include "Integer.h"
 
