4c50957a2da721fce8e68e1a2e974464880d0ba0
Philipp Paulweber <p.paulweber@gmail.com>
2018-09-25 11:38:52 +0200

    C++ Math: provided basic min and max functionality

--- src/cpp/Math.h
+++ src/cpp/Math.h
@@ -69,6 +69,18 @@ namespace libstdhl
         {
             return ( v != 0 ) ? gcd( v, u % v ) : u;
         }
+
+        template < typename T >
+        static inline const T& min( const T& a, const T& b )
+        {
+            return ( a < b ) ? a : b;
+        }
+
+        template < typename T >
+        static inline const T& max( const T& a, const T& b )
+        {
+            return ( a < b ) ? b : a;
+        }
     };
 }
 
