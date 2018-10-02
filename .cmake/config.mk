d2d0ae8ee12514748584841dcdce73305babf91c
Philipp Paulweber <p.paulweber@gmail.com>
2018-10-02 12:00:18 +0200

    Configuration: updated make/cmake integration and configured source-based install path option

--- .cmake/config.mk
+++ .cmake/config.mk
@@ -366,7 +366,11 @@ ALL   = $(TYPES:%=%-all)
 
 ENV_CMAKE_FLAGS  = -G$(ENV_GEN)
 ENV_CMAKE_FLAGS += -DCMAKE_BUILD_TYPE=$(TYPE)
-ENV_CMAKE_FLAGS += -DCMAKE_INSTALL_PREFIX=$(BIN)
+
+ifndef I
+  I = $(BIN)
+endif
+ENV_CMAKE_FLAGS += -DCMAKE_INSTALL_PREFIX=$(I)
 
 ifeq (,$(findstring Visual,$(ENV_GEN)))
   ENV_CMAKE_FLAGS += -DCMAKE_C_COMPILER=$(ENV_CC)
@@ -414,6 +418,7 @@ else
   endif
 endif
 
+
 ENV_BUILD_FLAGS  =
 ifneq (,$(findstring Makefile,$(ENV_GEN)))
   ENV_BUILD_FLAGS += --no-print-directory $(MFLAGS)
@@ -454,7 +459,7 @@ test: debug-test
 test-all: $(TYPES:%=%-test)
 
 $(TESTS):%-test: %
-	@cmake --build $(OBJ) --config $(patsubst %-sync,%,$@) --target $(TARGET)-check -- $(ENV_BUILD_FLAGS)
+	@cmake --build $(OBJ) --config $(patsubst %-test,%,$@) --target $(TARGET)-check -- $(ENV_BUILD_FLAGS)
 ifeq ($(ENV_CC),emcc)
 	cd ./$(OBJ) && \
 	`cat CMakeFiles/$(TARGET)-check.dir/link.txt | \
@@ -471,7 +476,7 @@ benchmark: debug-benchmark
 benchmark-all: $(TYPES:%=%-benchmark)
 
 $(BENCH):%-benchmark: %
-	@cmake --build $(OBJ) --config $(patsubst %-sync,%,$@) --target $(TARGET)-run -- $(ENV_BUILD_FLAGS)
+	@cmake --build $(OBJ) --config $(patsubst %-benchmark,%,$@) --target $(TARGET)-run -- $(ENV_BUILD_FLAGS)
 ifeq ($(ENV_CC),emcc)
 	cd ./$(OBJ) && \
 	`cat CMakeFiles/$(TARGET)-run.dir/link.txt | \
@@ -488,7 +493,7 @@ install: debug-install
 install-all: $(TYPES:%=%-install)
 
 $(INSTA):%-install: %
-	@cmake --build $(OBJ) --config $(patsubst %-sync,%,$@) --target install -- $(ENV_BUILD_FLAGS)
+	@cmake --build $(OBJ) --config $(patsubst %-install,%,$@) --target install -- $(ENV_BUILD_FLAGS)
 
 
 format: $(FORMAT:%=%-format-cpp)
