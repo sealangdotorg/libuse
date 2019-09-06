c143763745800909c7b07ea7ea980ba9b33752d5
Philipp Paulweber <p.paulweber@gmail.com>
2019-09-06 18:14:54 +0200

    C++ Unicode: added block, plane, and range abstractions
    * primer support of common Unicode planes and block ranges
    * added functionality to test if UTF-8 characters are inside certain
      block ranges
    * provided proper unit tests

--- src/cpp/Unicode.h
+++ src/cpp/Unicode.h
@@ -42,8 +42,11 @@
 //
 
 #pragma once
-#ifndef _LIBSTDHL_H_
-#define _LIBSTDHL_H_
+#ifndef _LIBSTDHL_CPP_UNICODE_H_
+#define _LIBSTDHL_CPP_UNICODE_H_
+
+#include <libstdhl/Type>
+#include <libstdhl/std/rfc3629>
 
 /**
    @brief    TODO
@@ -51,38 +54,71 @@
    TODO
 */
 
-#include <libstdhl/Allocator>
-#include <libstdhl/Ansi>
-#include <libstdhl/Args>
-#include <libstdhl/Binding>
-#include <libstdhl/Enum>
-#include <libstdhl/Environment>
-#include <libstdhl/Exception>
-#include <libstdhl/File>
-#include <libstdhl/Hash>
-#include <libstdhl/Json>
-#include <libstdhl/Labeling>
-#include <libstdhl/List>
-#include <libstdhl/Log>
-#include <libstdhl/Memory>
-#include <libstdhl/Network>
-#include <libstdhl/Random>
-#include <libstdhl/SourceLocation>
-#include <libstdhl/Stack>
-#include <libstdhl/Standard>
-#include <libstdhl/String>
-#include <libstdhl/Type>
-#include <libstdhl/Unicode>
-#include <libstdhl/Variadic>
-#include <libstdhl/Version>
-#include <libstdhl/Xml>
-#include <libstdhl/Yaml>
-
 namespace libstdhl
 {
+    /**
+       @extends Stdhl
+    */
+    namespace Unicode
+    {
+        using UTF8 = Standard::RFC3629::UTF8;
+
+        enum class Plane
+        {
+            BASIC_MULTILINGUAL = 0,
+            SUPPLEMENTARY_MULTILINGUAL = 1,
+            SUPPLEMENTARY_IDEOGRAPHIC = 2,
+            // 3-13 unassigned planes
+        };
+
+        /**
+           https://en.wikipedia.org/wiki/Unicode_block
+         */
+        enum class Block
+        {
+            // 0 BMP
+            // U+2600..U+26FF | Miscellaneous Symbols | 256 | 256 | Common
+            MISCELLANEOUS_SYMBOLS = 0x2600,
+            // U+2700..U+27BF | Dingbats | 192 | 192 | Common
+            DINGBATS = 0x2700,
+
+            // 1 SMP
+            // U+1F300..U+1F5FF | Miscellaneous Symbols and Pictographs | 768 | 768 | Common
+            MISCELLANEOUS_SYMBOLS_AND_PICTOGRAPHS = 0x1f300,
+            // U+1F600..U+1F64F | Emoticons | 80 | 80 | Common
+            EMOTICONS = 0x1f600,
+            // U+1F650..U+1F67F | Ornamental Dingbats | 48 | 48 | Common
+            ORNAMENTAL_DINGBATS = 0x1f650,
+            // U+1F680..U+1F6FF | Transport and Map Symbols | 128 | 110 | Common
+            TRANSPORT_AND_MAP_SYMBOLS = 0x1f680,
+        };
+
+        u1 inside( const UTF8& character, const std::vector< Unicode::Block >& blocks );
+
+        class Range final
+        {
+          public:
+            Range( const u32 start, const u32 end, const Plane plane );
+
+            const u32 start( void ) const;
+
+            const u32 end( void ) const;
+
+            const Plane plane( void ) const;
+
+          private:
+            const u32 m_start;
+            const u32 m_end;
+            const Plane m_plane;
+
+          public:
+            static Range of( const Block block );
+        };
+
+    };
 }
 
-#endif  // _LIBSTDHL_H_
+#endif  // _LIBSTDHL_CPP_UNICODE_H_
 
 //
 //  Local variables:
