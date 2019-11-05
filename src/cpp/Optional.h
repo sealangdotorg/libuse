b6f9e84cdc9d608e073f535be202553c83912cd0
Philipp Paulweber <p.paulweber@gmail.com>
2019-11-05 02:48:16 +0100

    Optional: reverted changes for specific AppleClang support
    * see: 7c2df47c5bd8304e358cbe6ee10caeffd2458c82

--- src/cpp/Optional.h
+++ src/cpp/Optional.h
@@ -46,14 +46,7 @@
 #define _LIBSTDHL_CPP_OPTIONAL_H_
 
 #include <exception>
-
-#if defined( __clang__ ) and defined( __apple_build_version__ )
-#include <optional>
-namespace StdOptional = std;
-#else
 #include <experimental/optional>
-namespace StdOptional = std::experimental;
-#endif
 
 /**
    @brief    TODO
@@ -68,12 +61,12 @@ namespace libstdhl
        @class Optional
      */
     template < typename T >
-    class Optional : public StdOptional::optional< T >
+    class Optional : public std::experimental::optional< T >
     {
       public:
         template < typename... Args >
         Optional( Args&&... args )
-        : StdOptional::optional< T >{ std::forward< Args >( args )... }
+        : std::experimental::optional< T >{ std::forward< Args >( args )... }
         {
         }
 
@@ -101,7 +94,7 @@ namespace libstdhl
         }
     };
 
-    constexpr auto nullopt = StdOptional::nullopt;
+    constexpr auto nullopt = std::experimental::nullopt;
 }
 
 #endif  // _LIBSTDHL_CPP_OPTIONAL_H_
